7667d9a2cf6f87c85a4a422449fff0ac6573afbe
NK49zx <khan49nibir03@gmail.com>
2025-03-10 23:45:14 +0600

    revised

--- jstyles2.js
+++ jstyles2.js
@@ -1,6 +1,6 @@
-const b1 = document.getElementById("b1");
-const b2 = document.getElementById("b2");
-const b3 = document.getElementById("b3");
+let b1 = document.getElementById("b1");
+let b2 = document.getElementById("b2");
+let b3 = document.getElementById("b3");
 
 b1.addEventListener('click', startTime);
 b2.addEventListener('click', stopTime);
@@ -14,10 +14,11 @@ function startTime(){ //Line 12-21: This function is used to trigger the Start a
     timeInt = setInterval(function() {
         document.getElementById('watch').innerHTML = zeroAdd(seconds);
         seconds+=3;
-        if (seconds >= 30){
-            clearInterval(timeInt);  
-            timeInt = null; 
-                   
+        if (seconds > 30){
+            clearInterval(timeInt);
+            b1.style.pointerEvents = 'none';  //Disables Start and Pause button when timer hits 30
+            b2.style.pointerEvents = 'none';
+            document.getElementById('r').style.color = 'maroon';    
         }
     }, 1000);
 }
@@ -26,14 +27,16 @@ function zeroAdd(t){  //Line 23-28: Adds 0 to the start of the count if the numb
     if (t<10){
         t = "0"+ t;
     }
-    return t
+    return t;
 }
 
 function resetTime(){  //Line 30-35: This function is used to trigger the Reset button of the Stopwatch.
     document.getElementById('watch').innerHTML = "00";
-    stopTime() 
-    seconds = 0
-    
+    stopTime() ;
+    seconds = 0;
+    b1.style.pointerEvents = 'auto';   //Enables Start and Pause button after resetting.
+    b2.style.pointerEvents = 'auto';
+    document.getElementById('r').style.color = 'black';
 }
 
 function stopTime(){  //Line 37-41: This function is used to trigger the Pause button of the Stopwatch.
